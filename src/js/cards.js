b43620504c5aad611ed89a21425333141285192e
stephane.malho <stepahen.malho@gmail.com>
2023-06-26 10:47:52 +0200

    🚨 fno > for loop & var & createElement

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,52 +1,92 @@
 function createRecipeCard(recipe) {
   var recipeCard = document.createElement("figure");
-  recipeCard.className = "card__image-container";
+  recipeCard.classList.add("card__image-container");
 
-  var ingredientsList = "";
+  var ingredientsList = document.createElement("ul");
+  ingredientsList.classList.add("card__tags");
+
   for (var i = 0; i < recipe.ingredients.length; i++) {
     var ingredient = recipe.ingredients[i];
+
+    var ingredientItem = document.createElement("li");
+
     var ingredientText = ingredient.ingredient;
 
     if (ingredient.quantity) {
-      ingredientText += ": <span>" + ingredient.quantity;
+      ingredientText += ": ";
 
-      if (ingredient.unit) {
-        ingredientText += " " + ingredient.unit + "</span>";
-      } else {
-        ingredientText += "</span>";
-      }
+      var quantitySpan = document.createElement("span");
+      quantitySpan.textContent = ingredient.quantity;
+
+      ingredientText += ingredient.unit ? ingredient.unit : "";
+      ingredientText += "</span>";
+      quantitySpan.innerHTML = ingredientText;
+
+      ingredientItem.appendChild(quantitySpan);
+    } else {
+      ingredientItem.textContent = ingredientText;
     }
 
-    ingredientsList += "<li>" + ingredientText + "</li>";
+    ingredientsList.appendChild(ingredientItem);
   }
 
-  recipeCard.innerHTML = "<figcaption class=\"card__image-caption\">" +
-    "<span class=\"timer-info\">" + recipe.time + " min</span>" +
-    "<img src=\"" + recipe.image + "\" alt=\"" + recipe.name + "\" class=\"card__image\" />" +
-    "</figcaption>" +
-    "<article class=\"card__content\">" +
-    "<h2 class=\"card__title\">" + recipe.name + "</h2>" +
-    "<h3 class=\"card__title__h3\">Recette</h3>" +
-    "<p class=\"card__description\">" + recipe.description + "</p>" +
-    "<h3>Ingrédients</h3>" +
-    "<ul class=\"card__tags\">" +
-    ingredientsList +
-    "<li id=\"card-tag\">" + recipe.servings + " personnes</li>" +
-    "</ul>" +
-    "</article>";
+  var cardCaption = document.createElement("figcaption");
+  cardCaption.classList.add("card__image-caption");
+
+  var timerInfoSpan = document.createElement("span");
+  timerInfoSpan.classList.add("timer-info");
+  timerInfoSpan.textContent = recipe.time + " min";
+
+  var image = document.createElement("img");
+  image.src = recipe.image;
+  image.alt = recipe.name;
+  image.classList.add("card__image");
+
+  cardCaption.appendChild(timerInfoSpan);
+  cardCaption.appendChild(image);
+
+  var cardContent = document.createElement("article");
+  cardContent.classList.add("card__content");
+
+  var cardTitle = document.createElement("h2");
+  cardTitle.classList.add("card__title");
+  cardTitle.textContent = recipe.name;
+
+  var cardTitleH3 = document.createElement("h3");
+  cardTitleH3.classList.add("card__title__h3");
+  cardTitleH3.textContent = "Recette";
+
+  var cardDescription = document.createElement("p");
+  cardDescription.classList.add("card__description");
+  cardDescription.textContent = recipe.description;
+
+  var servingsItem = document.createElement("li");
+  servingsItem.id = "card-tag";
+  servingsItem.textContent = recipe.servings + " personnes";
+
+  var ingredientsHeading = document.createElement("h3");
+  ingredientsHeading.textContent = "Ingrédients";
+
+  cardContent.appendChild(cardTitle);
+  cardContent.appendChild(cardTitleH3);
+  cardContent.appendChild(cardDescription);
+  cardContent.appendChild(ingredientsHeading);
+  cardContent.appendChild(ingredientsList);
+  cardContent.appendChild(servingsItem);
+
+  recipeCard.appendChild(cardCaption);
+  recipeCard.appendChild(cardContent);
 
   return recipeCard;
 }
 
 function renderRecipes(recipesList) {
-  var container = document.querySelector(".cards__container");
-  // Effacer le contenu existant dans le conteneur
+  var container = document.getElementsByClassName("cards__container")[0]; // selection par class moins performant que par querySelector
   container.innerHTML = "";
-  // Parcourir les recettes et générer le contenu HTML
   for (var i = 0; i < recipesList.length; i++) {
+    // boucle for plus performante que forEach mais moins lisible
     var recipe = recipesList[i];
     var recipeCard = createRecipeCard(recipe);
-    // Ajoutez d'autres éléments HTML pour afficher les autres données de la recette, si nécessaire
     container.appendChild(recipeCard);
   }
 }
